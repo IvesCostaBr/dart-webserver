2150b0a9c5e79e32200ad476cc7041391089bf64
Ives Costa <35783814+IvesCostaBr@users.noreply.github.com>
2021-09-08 13:30:16 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
 main() {
-  HttpServer
-      .bind(InternetAddress.anyIPv6, 80)
-      .then((server) {
-        server.listen((HttpRequest request) {
-          request.response.write('Hi, i'am server http in dart:)!');
-          request.response.close();
-        });
-      });
+  HttpServer.bind("localhost", 8000).then((HttpServer server) {
+	server.listen((HttpRequest request) {
+	request.response.write("Hi, seerver dart");
+	request.response.close();
+	});
+     });
+}
+});
 }
